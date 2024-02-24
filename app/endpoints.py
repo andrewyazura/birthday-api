b1ace1a97982cb336239455e3435329578c25ffe
orehzzz <olegzzz749@gmail.com>
2024-02-24 22:17:56 +0200

    try/except on all endpoints

--- app/endpoints.py
+++ app/endpoints.py
@@ -14,6 +14,7 @@ from flask_jwt_extended import (
 import datetime
 from werkzeug.exceptions import HTTPException
 from marshmallow import ValidationError
+from peewee import DoesNotExist, IntegrityError
 
 TELEGRAM_BOT_TOKEN = config.get("Main", "telegram_bot_token")
 JWT_EXPIRES_MINUTES = int(config.get("Main", "jwt_expires_minutes"))
@@ -23,14 +24,20 @@ JWT_EXPIRES_MINUTES = int(config.get("Main", "jwt_expires_minutes"))
 def telegram_login():
     if not _check_telegram_data(request.args.to_dict()):
         abort(412, description="Bad credentials from login via Telegram")
-    user, created = Users.get_or_create(telegram_id=request.args.get("id"))
-    identity = {"telegram_id": user.telegram_id, "admin": "False"}
-    jwt_token = create_access_token(
-        identity=identity, expires_delta=datetime.timedelta(minutes=JWT_EXPIRES_MINUTES)
-    )
-    response = Response(status=200)
-    set_access_cookies(response, jwt_token)
-    return response
+    try:
+        user, created = Users.get_or_create(telegram_id=request.args.get("id"))
+        identity = {
+            "telegram_id": user.telegram_id
+        }  # incoming birthdays endpoint will check if telegram_id is in the config["Admins"]
+        jwt_token = create_access_token(
+            identity=identity,
+            expires_delta=datetime.timedelta(minutes=JWT_EXPIRES_MINUTES),
+        )
+        response = Response(status=200)
+        set_access_cookies(response, jwt_token)
+        return response
+    except Exception as error:
+        abort(500, description=f"Unexpected {error=}")
 
 
 def _check_telegram_data(data_dict):
@@ -53,7 +60,7 @@ def _check_telegram_data(data_dict):
 @app.route("/logout")
 @jwt_required()
 def logout():
-    response = jsonify({"msg": "logout successful"})
+    response = Response(status=200)
     unset_jwt_cookies(response)
     return response
 
@@ -61,71 +68,86 @@ def logout():
 @app.route("/birthdays", methods=["GET"])
 @jwt_required()
 def users_birthdays():
-    current_user = get_jwt_identity()
-    user = Users.get(telegram_id=current_user["telegram_id"])
-    birthdays = Users.get(Users.telegram_id == user.telegram_id).birthdays
-    response = jsonify([model_to_dict(birthday) for birthday in birthdays])
-    return response
+    try:
+        current_user = get_jwt_identity()
+        user = Users.get(telegram_id=current_user["telegram_id"])
+        birthdays = Users.get(Users.telegram_id == user.telegram_id).birthdays
+        data = [model_to_dict(birthday) for birthday in birthdays]
+        if data == []:
+            raise DoesNotExist
+        return jsonify(data), 200
+    except DoesNotExist:
+        abort(404, description="There are no birthdays for this user")
+    except Exception as error:
+        abort(500, description=f"Unexpected {error=}")
 
 
-@app.route("/birthdays/<id>", methods=["GET"])
+@app.route("/birthdays/<int:id>", methods=["GET"])
 @jwt_required()
 def one_birthday(id):
-    current_user = get_jwt_identity()
-    user, created = Users.get_or_create(telegram_id=current_user["telegram_id"])
-    birthday = Birthdays.get((Birthdays.creator == user) & (Birthdays.id == id))
-    return jsonify(model_to_dict(birthday))
+    try:
+        current_user = get_jwt_identity()
+        user = Users.get(telegram_id=current_user["telegram_id"])
+        birthday = Birthdays.get((Birthdays.creator == user) & (Birthdays.id == id))
+        return jsonify(model_to_dict(birthday)), 200
+    except DoesNotExist:
+        abort(404, description="Birthday not found")
+    except Exception as error:
+        abort(500, description=f"Unexpected {error=}")
 
 
-@app.route("/birthdays", methods=["POST"])  # handle errors
+@app.route("/birthdays", methods=["POST"])
 @jwt_required()
 def add_birthday():
-    current_user = get_jwt_identity()
-    user = Users.get(telegram_id=current_user["telegram_id"])
-    data = request.get_json()
-    # add my own birthday for every new user:) if so, its better to make another request from frontend/bot
-    birthday_id = Birthdays.create(
-        name=data.get("name"),
-        day=data.get("day"),
-        month=data.get("month"),
-        year=data.get("year"),  # .get() returns none if no key in dict
-        note=data.get("note"),
-        creator=user,
-    )
-    response = jsonify(model_to_dict(Birthdays.get_by_id(birthday_id)))
-    return response, 201
+    try:
+        data = birthdays_schema.load(request.get_json())
+        current_user = get_jwt_identity()
+        user = Users.get(telegram_id=current_user["telegram_id"])
+        # add my own birthday for every new user:) if so, its better to make another request from frontend/bot
+        birthday_id = Birthdays.create(
+            name=data.get("name"),
+            day=data.get("day"),
+            month=data.get("month"),
+            year=data.get("year"),  # .get() returns none if no key in dict
+            note=data.get("note"),
+            creator=user,
+        )
+        response = jsonify(model_to_dict(Birthdays.get_by_id(birthday_id)))
+        return response, 201
+    except ValidationError as error:
+        print(error.messages)
+        abort(422, description=error.messages)
+    except IntegrityError:
+        abort(422, description="User already has a birthday with this name")
+    except Exception as error:
+        abort(500, description=f"Unexpected {error=}")
 
 
-@app.route("/birthdays/<id>", methods=["DELETE"])
+@app.route("/birthdays/<int:id>", methods=["DELETE"])
 @jwt_required()
 def delete_birthday(id):
-    current_user = get_jwt_identity()
-    user = Users.get(telegram_id=current_user["telegram_id"])
-    if (
-        not Birthdays.delete()
-        .where((Birthdays.creator == user) & (Birthdays.id == id))
-        .execute()
-    ):
-        response = Response(status=404)
-        return response
-    response = Response(status=204)
-    return response
+    try:
+        current_user = get_jwt_identity()
+        user = Users.get(telegram_id=current_user["telegram_id"])
+        Birthdays.get(
+            (Birthdays.creator == user) & (Birthdays.id == id)
+        ).delete_instance()
+        return Response(status=204)
+    except DoesNotExist:
+        abort(404, description="Can't delete non-existent birthday")
+    except Exception as error:
+        abort(500, description=f"Unexpected {error=}")
 
 
 @app.route(
-    "/birthdays/<id>", methods=["PUT"]
+    "/birthdays/<int:id>", methods=["PUT"]
 )  # PATCH contains only new info, PUT - new object to replace with
 @jwt_required()
 def update_birthday(id):
     try:
         data = birthdays_schema.load(request.get_json())
-    except ValidationError as error:
-        abort(422, description=error.messages["_schema"])
-    # current_user = get_jwt_identity()
-    user, created = Users.get_or_create(
-        telegram_id=651472384
-    )  # current_user["telegram_id"]
-    try:
+        current_user = get_jwt_identity()
+        user = Users.get(telegram_id=current_user["telegram_id"])
         Birthdays.update(
             name=data.get("name"),
             day=data.get("day"),
@@ -133,15 +155,22 @@ def update_birthday(id):
             year=data.get("year"),
             note=data.get("note"),
         ).where((Birthdays.creator == user) & (Birthdays.id == id)).execute()
-    except Exception as exception:
-        abort(404, description="Birthday not found, can't update")
-    response = jsonify(model_to_dict(Birthdays.get_by_id(id)))
-    return response, 200
+        response = jsonify(
+            model_to_dict(Birthdays.get_by_id(id))
+        )  # comes without id - response with id
+        return response, 200
+    except ValidationError as error:
+        abort(422, description=error.messages)
+    except DoesNotExist:
+        abort(404, description="Can't update non-existent birthday")
+    except IntegrityError:
+        abort(422, description="User already has a birthday with this name")
+    except Exception as error:
+        abort(500, description=f"Unexpected {error=}")
 
 
 @app.errorhandler(HTTPException)
-def handle_exception(e):
-    # headers = [("Access-Control-Allow-Credentials", "true")]
+def general_exception_handler(e):
     response = make_response(
         {
             "status": "error",
@@ -150,12 +179,9 @@ def handle_exception(e):
             "description": e.description,
         },
         e.code,
-        # headers,
     )
     response.content_type = "application/json"
-    app.logger.exception(
-        f"{datetime.datetime.now()}\n{response.get_data(as_text=True)}"
-    )
+    app.logger.error(f"{datetime.datetime.now()}:  {response.get_data(as_text=True)}")
     return response
 
 
